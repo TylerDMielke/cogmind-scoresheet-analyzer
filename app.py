7d873f578cc95628d50d2be43295faba2b7a556c
Tyler Mielke <dummy@dumdum.com>
2023-06-18 20:28:18 -0500

    little tweaks

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, Tab, Tabs
+import textual.widgets as widgets
 
 from cogmind_scoresheet_analyzer.config import DEV_SCORESHEET_DIRECTORY
 from cogmind_scoresheet_analyzer.scoresheet import Scoresheet
@@ -19,23 +19,24 @@ class CogmindScoresheetAnalyzerApp(App):
         ("d", "toggle_dark", "Toggle dark mode"),
         ("q", "quit", "Close application"),
     ]
+    TITLE = "Cogmind Scoresheet Analyzer"
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
-        yield Header(name="Cogmind Scoresheet Analyzer")
-        yield self._create_tabs()
-        yield Footer()
+        yield widgets.Header()
+        yield self._create_scoresheet_list()
+        yield widgets.Footer()
     
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.dark = not self.dark
 
-    def _create_tabs(self) -> Tabs:
-        tab_list: list[Tab] = []
+    def _create_scoresheet_list(self) -> widgets.ListView:
+        items: list[widgets.ListItem] = []
         scoresheets: list[Scoresheet] = self.scoresheet_loader.load_scoresheets()
         for scoresheet in scoresheets:
-            tab_list.append(Tab(scoresheet.run_date))
-        return Tabs(*tab_list)
+            items.append(widgets.ListItem(widgets.Label(scoresheet.run_date)))
+        return widgets.ListView(*items)
 
 if __name__ == "__main__":
     app = CogmindScoresheetAnalyzerApp()
--- cogmind_scoresheet_analyzer/scoresheet_view.py
+++ cogmind_scoresheet_analyzer/scoresheet_view.py
@@ -1,5 +1,22 @@
 from textual.app import ComposeResult
-from textual.widgets import Header, Footer, Static
+from textual.widgets import Footer, Header, Label, Static
+
+from cogmind_scoresheet_analyzer.scoresheet import Scoresheet
+
 
 class ScoreSheet(Static):
-    pass
+    def __init__(self, scoresheet: Scoresheet):
+        super().__init__()
+        self.scoresheet = scoresheet
+
+    BINDINGS = [
+        ("d", "toggle_dark", "Toggle dark mode"),
+        ("q", "q", "Quit"),
+    ]
+
+    def compose(self) -> ComposeResult:
+        """Create child widgets for the app."""
+        yield Header()
+        yield Label(f"Played by: {self.scoresheet.player}: {self.scoresheet.run_date}", id="score_sheet_header")
+        yield Label(f"Result: {self.scoresheet.result}", id="result")
+        yield Footer()
